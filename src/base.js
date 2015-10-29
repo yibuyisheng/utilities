5bed29f1ba225e95d0b8914f966f8017357868a7
yibuyisheng <yibuyisheng@163.com>
2015-10-29 18:41:23 +0800

    Update base.js

--- src/base.js
+++ src/base.js
@@ -11,7 +11,18 @@ module.exports = {
     format: format,
     keys: keys,
     values: values,
-    isNumber: isNumber
+    isNumber: isNumber,
+    generateUUID: generateUUID
+};
+
+function generateUUID(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+    });
+    return uuid;
 };
 
 function getClassName(obj) {
